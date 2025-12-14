0d6c1f5f7ac5c5892f591dfe767aec92fc3584f3
Marian Hrytsan <gricanmaran@gmail.com>
2025-12-14 21:55:56 +0200

    error correction

--- app/main.py
+++ app/main.py
@@ -11,13 +11,7 @@ def format_linter_error(error: dict) -> dict:
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [
-            {
-                "line": data_error["line_number"],
-                "column": data_error["column_number"],
-                "message": data_error["text"],
-                "name": data_error["code"],
-                "source": "flake8",
-            }
+            format_linter_error(data_error)
             for data_error in errors
             if data_error["filename"] == file_path
         ],
@@ -28,20 +22,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [
-                {
-                    "line": err["line_number"],
-                    "column": err["column_number"],
-                    "message": err["text"],
-                    "name": err["code"],
-                    "source": "flake8",
-                }
-                for err in data_error
-                if any(data_error)
-            ],
-            "path": road_to_file,
-            "status": "failed" if any(data_error) else "passed",
-        }
+        format_single_linter_file(road_to_file, data_error)
         for road_to_file, data_error in linter_report.items()
     ]
